21aa4c29c2e578bc8f4c526746a47d55f8d53809
Mark Borkum <mark.borkum@pnnl.gov>
2019-03-08 17:34:35 -0800

    reuse globals in tests

--- pacifica/notifications/client/tests/test_event_handlers.py
+++ pacifica/notifications/client/tests/test_event_handlers.py
@@ -16,14 +16,15 @@ from cloudevents.constants import SPEC_VERSION
 from cloudevents.model import Event
 
 from pacifica.notifications.client.event_handlers import NoopEventHandler
+from pacifica.notifications.client.globals import CLOUDEVENTS_DEFAULT_EVENT_TYPE_, CLOUDEVENTS_DEFAULT_SOURCE_
 
 class NoopEventHandlerTestCase(unittest.TestCase):
     def test_event_handler(self):
         event = Event({
             'cloudEventsVersion': SPEC_VERSION,
-            'eventID': '1',
-            'eventType': 'example',
-            'source': '/example',
+            'eventID': 'ID',
+            'eventType': CLOUDEVENTS_DEFAULT_EVENT_TYPE_,
+            'source': CLOUDEVENTS_DEFAULT_SOURCE_,
             'data': [],
         })
 
